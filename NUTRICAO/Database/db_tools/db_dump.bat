d2dc0a0e1f19e1d12b43688accfbb53714c0f656
Leandro <Leandro@DESKTOP-84NF5B0>
2019-09-03 12:51:39 -0300

    Banco neo4j reupado apenas com o 'dump', junto com utilitários cmd/bat adicionais (restore/load).

---
+++ NUTRICAO/Database/db_tools/db_dump.bat
@@ -0,0 +1,16 @@
+@echo off
+for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined MyDate set MyDate=%%x
+for /f %%x in ('wmic path win32_localtime get /format:list ^| findstr "="') do set %%x
+set fmonth=00%Month%
+set fday=00%Day%
+set today=%Year%-%fmonth:~-2%-%fday:~-2%
+
+echo %today%
+
+
+SET hora=%time%
+SET timeok=%hora::=-%
+SET timeok=%timeok:,=-%
+@echo on
+
+neo4j-admin dump --database=graph.db --to=/neo4j/data/backups/graph.db/%today%-%timeok%.dump
---
+++ NUTRICAO/Database/db_tools/db_restore.bat
@@ -0,0 +1,1 @@
+neo4j-admin load --from=/neo4j/data/backups/graph.db/restore.dump --database=graph.db --force
