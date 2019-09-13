b9d25631764c87a9bcb9d28867882b4808a66e33
Leandro <Leandro@DESKTOP-84NF5B0>
2019-09-13 16:57:25 -0300

    Valor Nutricional.

--- NUTRICAO/src/main/java/com/iala/cpd/entity/ValorNutricional.java
+++ NUTRICAO/src/main/java/com/iala/cpd/entity/ValorNutricional.java
@@ -1,12 +1,21 @@
 package com.iala.cpd.entity;
 
+import org.neo4j.ogm.annotation.EndNode;
 import org.neo4j.ogm.annotation.GeneratedValue;
 import org.neo4j.ogm.annotation.Id;
-import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.RelationshipEntity;
+import org.neo4j.ogm.annotation.StartNode;
 import lombok.Data;
 
-@NodeEntity
+@RelationshipEntity("VALOR_NUTRICIONAL")
 @Data
 public class ValorNutricional {
 	@Id @GeneratedValue Long id;
+	private Float quantidade;
+	
+	@StartNode
+	private GeneroAlimento alimento;
+	
+	@EndNode
+	private Nutriente nutriente;
 }
