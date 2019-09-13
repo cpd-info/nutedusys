aab1e5c46df20a3365590c41c6ef523c6246502b
Leandro <Leandro@DESKTOP-84NF5B0>
2019-09-13 12:59:24 -0300

    Classe de relacionamento Setor -> Segmento.

---
+++ NUTRICAO/src/main/java/com/iala/cpd/entity/QuantitativoSegmento.java
@@ -0,0 +1,21 @@
+package com.iala.cpd.entity;
+
+import org.neo4j.ogm.annotation.EndNode;
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.RelationshipEntity;
+import org.neo4j.ogm.annotation.StartNode;
+import lombok.Data;
+
+@RelationshipEntity("COMPOSTO_POR")
+@Data
+public class QuantitativoSegmento {
+	@Id @GeneratedValue Long id;
+	private Long quantidade_alunos;
+	
+	@StartNode
+	private Setor setor;
+	
+	@EndNode
+	private Segmento segmento;
+}
