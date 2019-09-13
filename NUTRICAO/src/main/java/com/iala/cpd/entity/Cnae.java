042f84ccfee647a15f6230152962dee6653ed285
cpd-info <54711707+cpd-info@users.noreply.github.com>
2019-09-13 19:09:03 -0300

    Tabela Cnae
    Tabela contendo os dados do código nacional de atividades econômicas.

---
+++ NUTRICAO/src/main/java/com/iala/cpd/entity/Cnae.java
@@ -0,0 +1,19 @@
+package com.iala.cpd.entity;
+
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+import lombok.Data;
+
+@NodeEntity
+@Data
+public class Cnae {
+	@Id @GeneratedValue Long id;
+        private String descricao;
+        private String anexo;
+        private String codigo;
+        private Boolean fator;
+        private Boolean atendido;
+        private Float aliquota;
+        private 
+}
