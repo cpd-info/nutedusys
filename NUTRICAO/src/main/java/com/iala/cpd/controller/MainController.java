91d53e3aa87e13d49b0e79356ed6663b71313560
Leandro <Leandro@DESKTOP-84NF5B0>
2019-09-13 16:56:37 -0300

    Limpo.

--- NUTRICAO/src/main/java/com/iala/cpd/controller/MainController.java
+++ NUTRICAO/src/main/java/com/iala/cpd/controller/MainController.java
@@ -3,7 +3,6 @@ package com.iala.cpd.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//import com.iala.cpd.repository.EstadoRepository;;
 
 @Controller
 public class MainController {
@@ -18,25 +17,8 @@ public class MainController {
 
 	@GetMapping(value = "/home")
 	public String home(Model model) {
-		//Organizacao org = organizacaoRepository.findAll(2).get(0);
-		//OrganizacaoModel orgMod = new OrganizacaoModel(org);
-		//Long ufId = org.getLocalidade().getEstado().getId();
-		//model.addAttribute("Organizacao", orgMod);
-		//model.addAttribute("Estados", estadoRepository.findAllByPais("Brasil"));
-		//model.addAttribute("Municipios", localidadeRepository.findByUfId(ufId));
 		return "index";
 	}
-
-
-	//@GetMapping(value = "/administracao")
-	//public String administracao() {
-	//	return "administracao";
-	//}
-
-	//@GetMapping(value = "/anoletivo")
-	//public String anoLetivo() {
-	//	return "ano_letivo";
-	//}
 
 	@GetMapping(value = "/acessoNegado")
 	public String acessoNegado() {
