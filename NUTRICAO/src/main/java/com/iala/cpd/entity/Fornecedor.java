ae5efd05499134f316659eadd23fe4340ff75f6a
Leandro <Leandro@DESKTOP-84NF5B0>
2019-09-16 09:26:10 -0300

    Adicionados imports.

--- NUTRICAO/src/main/java/com/iala/cpd/entity/Fornecedor.java
+++ NUTRICAO/src/main/java/com/iala/cpd/entity/Fornecedor.java
@@ -3,10 +3,15 @@ package com.iala.cpd.entity;
 import org.neo4j.ogm.annotation.GeneratedValue;
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
+
+import com.iala.cpd.entityAbstract.PessoaJuridica;
+
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @NodeEntity
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class Fornecedor extends PessoaJuridica{
 	@Id @GeneratedValue Long id;
 }
