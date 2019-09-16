c77e2536028e6c9c134ddf9bc8a700fef9463e20
Leandro <Leandro@DESKTOP-84NF5B0>
2019-09-16 16:02:41 -0300

    Removed unused import.

--- NUTRICAO/src/main/java/com/iala/cpd/entity/Config.java
+++ NUTRICAO/src/main/java/com/iala/cpd/entity/Config.java
@@ -3,7 +3,6 @@ package com.iala.cpd.entity;
 import org.neo4j.ogm.annotation.GeneratedValue;
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
-import org.neo4j.ogm.annotation.Relationship;
 import lombok.Data;
 
 @NodeEntity
