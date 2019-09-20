5373356061a754ed87555bb4f5566fb1642b138f
Leandro <Leandro@DESKTOP-84NF5B0>
2019-09-20 11:02:29 -0300

    Adicionada classe Ata que gera os contratos.

---
+++ NUTRICAO/src/main/java/com/iala/cpd/entity/Ata.java
@@ -0,0 +1,18 @@
+package com.iala.cpd.entity;
+
+import java.util.List;
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+import lombok.Data;
+
+@NodeEntity
+@Data
+public class Ata {
+	@Id @GeneratedValue Long id;
+	private Integer numero;
+	
+	@Relationship("LICITADO_PARA")
+	public List<Fornecedor> fornecedores;
+}
