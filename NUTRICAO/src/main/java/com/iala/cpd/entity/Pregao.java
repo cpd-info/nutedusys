1652972df1d422a4bbc9dea9c626ffd1159734f3
Leandro <Leandro@DESKTOP-84NF5B0>
2019-09-20 11:07:08 -0300

    Completada com parametros preeliminares.

--- NUTRICAO/src/main/java/com/iala/cpd/entity/Pregao.java
+++ NUTRICAO/src/main/java/com/iala/cpd/entity/Pregao.java
@@ -1,12 +1,24 @@
 package com.iala.cpd.entity;
 
+import java.util.List;
 import org.neo4j.ogm.annotation.GeneratedValue;
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
 import lombok.Data;
 
 @NodeEntity
 @Data
 public class Pregao {
 	@Id @GeneratedValue Long id;
+	
+	private Integer numero;
+	private Integer processo;
+	private Integer licitacao;
+	
+	@Relationship("ORIGINA")
+	public List<Ata> atas;
+	
+	@Relationship("PROCESSOU")
+	public DemonstrativoAquisicao aquisicao;
 }
