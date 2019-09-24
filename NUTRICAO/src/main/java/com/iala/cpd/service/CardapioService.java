e7ae156d50c321cda44a1cac5bf32b307fa8f283
Leandro <Leandro@DESKTOP-84NF5B0>
2019-09-24 09:36:55 -0300

    Inicializado para construção.

--- NUTRICAO/src/main/java/com/iala/cpd/service/CardapioService.java
+++ NUTRICAO/src/main/java/com/iala/cpd/service/CardapioService.java
@@ -1,16 +1,8 @@
 package com.iala.cpd.service;
 
-import java.util.List;
-import java.util.Map;
-import com.iala.cpd.type.DiaSemana;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CardapioService {	
-	
-	public String nome_segmento(){
-		return "";
-	};
-	
-	public List<Map<DiaSemana, String>> cardapios(){
-		return null;
-	};
+
 }
