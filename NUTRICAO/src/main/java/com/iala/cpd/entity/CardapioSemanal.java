2780f4f0c62f4e0567ab30a06017cd1f3cbbea22
Leandro <Leandro@DESKTOP-84NF5B0>
2019-10-01 09:25:34 -0300

    Implementa CrudBuilder

--- NUTRICAO/src/main/java/com/iala/cpd/entity/CardapioSemanal.java
+++ NUTRICAO/src/main/java/com/iala/cpd/entity/CardapioSemanal.java
@@ -2,19 +2,92 @@ package com.iala.cpd.entity;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.neo4j.ogm.annotation.GeneratedValue;
-import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
+import com.iala.cpd.entityAbstract.BaseEntity;
+import com.iala.cpd.serviceAbstract.Arrow;
+import com.iala.cpd.serviceAbstract.CrudBuilder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @NodeEntity
 @Data
-public class CardapioSemanal {
-	@Id @GeneratedValue Long id;
-	private String nome;
-	private String descricao;
+@EqualsAndHashCode(callSuper=false)
+public class CardapioSemanal extends BaseEntity implements CrudBuilder{
 	
-	@Relationship(type="MONTA", direction=Relationship.INCOMING)
-	private List<ComposicaoCardapio> pratos = new ArrayList<>();
+	@EqualsAndHashCode.Exclude	
+	@Relationship("MONTADO_COM")
+	private List<ComposicaoCardapio> pratos = new ArrayList<ComposicaoCardapio>();
+	
+	public CardapioSemanal(String nome, String descricao) {
+		this.setNome(nome);
+		this.setDescricao(descricao);
+	}
+
+	@Override
+	public void addTo(Arrow Arrow) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeFrom(Arrow Arrow) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setGeneroAlimento(GeneroAlimento generoAlimento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setProduto(Produto produto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setMenuDiario(MenuDiario menuDiario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setCardapioSemanal(CardapioSemanal cardapioSemanal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public GeneroAlimento getGeneroAlimento() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Produto getProduto() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MenuDiario getMenuDiario() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CardapioSemanal getCardapioSemanal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CardapioSemanal getThis() {
+		// TODO Auto-generated method stub
+		return this;
+	}
+
 }
