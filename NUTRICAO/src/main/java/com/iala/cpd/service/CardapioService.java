258167f036e6def493c4b3915bd57f68e9ef2680
Leandro <Leandro@DESKTOP-84NF5B0>
2019-10-01 09:39:17 -0300

    Remoção do repositório Dao, em face da diretriz 760ad05cd42a22b5a1e0214ff25206d2976693cf

--- NUTRICAO/src/main/java/com/iala/cpd/service/CardapioService.java
+++ NUTRICAO/src/main/java/com/iala/cpd/service/CardapioService.java
@@ -3,16 +3,16 @@ package com.iala.cpd.service;
 import java.io.Serializable;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Service;
 import com.iala.cpd.entityAbstract.BaseEntity;
-import com.iala.cpd.repository.Dao;
 import com.iala.cpd.serviceAbstract.Arrow;
 
 @Service
 public class CardapioService<S extends BaseEntity, ID extends Serializable> {
 
 	@Autowired
-	private Dao<S, ID> customDao;
+	private Neo4jRepository<S, ID> customDao;
 	
 	public void novo(S entity) {
 		customDao.save(entity);
