7e9bfdda621ddad3ed655267dd19244610def561
Leandro <Leandro@DESKTOP-84NF5B0>
2019-10-01 09:36:00 -0300

    Testes com IntStream.Range no lugar do loop 'for' Java padrão.

--- NUTRICAO/src/main/java/com/iala/cpd/service/DemonstrativoService.java
+++ NUTRICAO/src/main/java/com/iala/cpd/service/DemonstrativoService.java
@@ -1,32 +1,29 @@
 package com.iala.cpd.service;
 
 import java.util.List;
+import java.util.stream.IntStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 import com.iala.cpd.entity.Setor;
 import com.iala.cpd.entity.TotalPorProduto;
-import com.iala.cpd.repository.SetorRepository;
+import com.iala.cpd.repository.TotalPorProdutoRepository;
 
 @Service
 public class DemonstrativoService {	
 	@Autowired
-	private SetorRepository setorRepository;
+	private TotalPorProdutoRepository totalPorProdutoRepository;
 
-	public Table<String, String, String> getDemonstrativo(Setor escola, Integer anoLetivo) {
+	public Table<Integer, String, String> getDemonstrativo(Setor escola, Integer anoLetivo) {
 
-		List<TotalPorProduto> listaTotalPorProduto = this.setorRepository.getTotPorProdList(escola.getId(), anoLetivo);
-		Table<String, String, String> tabela = HashBasedTable.create();
-		listaTotalPorProduto.forEach(e -> {
-			Integer i=0, j;
-			j=i++;
-			tabela.put(j.toString(), "Descrição", e.getProduto().getDescricao());
-			tabela.put(j.toString(), "Quantidade", e.getQtd().toString());
-			e.getProduto().getDescricao();
+		List<TotalPorProduto> listaTotalPorProduto = this.totalPorProdutoRepository.getTotPorProdList(escola.getId(), anoLetivo);
+		Table<Integer, String, String> tabela = HashBasedTable.create();
+		IntStream.range(0, listaTotalPorProduto.size()+1).forEach(i -> {
+			tabela.put(i, "Descrição", listaTotalPorProduto.get(i).getProduto().getDescricao());
+			tabela.put(i, "Quantidade", listaTotalPorProduto.get(i).getQtd().toString());
 		});
-
 		return tabela;
-
 	}
 }
