e76f72d7305e7f5b6b84c88976e8546edb5c2e42
Leandro <Leandro@DESKTOP-84NF5B0>
2019-10-01 09:15:47 -0300

    AlimentoDiario extende BaseEntity e Arrow

--- NUTRICAO/src/main/java/com/iala/cpd/entity/AlimentoDiario.java
+++ NUTRICAO/src/main/java/com/iala/cpd/entity/AlimentoDiario.java
@@ -1,22 +1,63 @@
 package com.iala.cpd.entity;
 
 import org.neo4j.ogm.annotation.EndNode;
-import org.neo4j.ogm.annotation.GeneratedValue;
-import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.RelationshipEntity;
 import org.neo4j.ogm.annotation.StartNode;
+import com.iala.cpd.entityAbstract.BaseEntity;
+import com.iala.cpd.serviceAbstract.Arrow;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @RelationshipEntity("COMPOSTO_POR")
 @Data
-public class AlimentoDiario {
-	@Id @GeneratedValue Long id;
-	private Float quantidade;
-	private Float quantidade_disfagia;
+@EqualsAndHashCode(callSuper=false)
+public class AlimentoDiario extends BaseEntity implements Arrow {
+	private Double quantidade;
+	private Double quantidade_disfagia;
 	
 	@StartNode
-	private GeneroAlimento generoAlimento;
+	private MenuDiario menuDiario;
 	
 	@EndNode
-	private MenuDiario menuDiario;
+	private GeneroAlimento generoAlimento;
+	
+	public AlimentoDiario(Double quantidade,Double quantidade_disfagia, MenuDiario menuDiario, GeneroAlimento generoAlimento) {
+		this.quantidade=quantidade;
+		this.quantidade_disfagia=quantidade_disfagia;
+		this.menuDiario=menuDiario;
+		this.generoAlimento=generoAlimento;
+	}
+
+	public CardapioSemanal getCardapioSemanal() {
+		return null;
+	}
+
+	public void setCardapioSemanal(CardapioSemanal cardapioSemanal) {
+		
+	}
+
+	@Override
+	public Setor getSetor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setSetor(Setor setor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Nutriente getNutriente() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setNutriente(Nutriente nutriente) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
